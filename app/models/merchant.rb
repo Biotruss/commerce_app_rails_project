54c1c097635d8febab2f1c06b3e3748a0235f3f6
Craig Messi <craigmessi@Craigs-MacBook-Pro.local>
2020-06-01 11:42:41 -0700

    updated associations in models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,6 @@
 class Merchant < ApplicationRecord
+  has_many :requests
+  has_many :users, through: :requests
+  has_many :products
   belongs_to :company
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers:[:facebook]
 
+  has_many :requests
+  has_many :merchants, through: :requests
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
